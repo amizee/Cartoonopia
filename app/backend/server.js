fe5b5bd416f66476f79b45508adac6995bbf431b
Josh <joli6799@uni.sydney.edu.au>
2024-04-28 19:51:38 +1000

    basic connection to db

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,44 +1,18 @@
-const path = require('path');
-const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+main().catch(err => console.log(err));
 
-/* db stuff? */
-mongoose.connect('mongodb+srv://kevin:hGDMKGJ0cZVWonkG@cluster0.v5i7fte.mongodb.net/');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB');
-});
+async function main() {
+    await mongoose.connect('mongodb+srv://kevin:hGDMKGJ0cZVWonkG@cluster0.v5i7fte.mongodb.net/characters');
+    const users = new mongoose.Schema({
+        firstname: String,
+        lastname: String,
+        email: String,
+        password: String
+        
+    });
 
-/* bruh i can't figure out how to query the db */
-const users = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String
-});
-
-const Users = mongoose.model('Users', users);
-/* const first = Users.find({firstname: 'Anita'}, function(err, docs) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('docs: ', docs)
-    }
-}); */
-//console.log(first);
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, '../frontend/views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-	res.render('index.ejs');
-});
-
-app.listen(3000, function () {
-	console.log('app listening on port 3000!');
-});
+    const Users = mongoose.model('Users', users);
+    const out = await Users.find();
+    console.log(out);
+}
