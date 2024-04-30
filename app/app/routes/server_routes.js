fc8574ab6320c2bdc9ccff727b0acb5f3f87c755
Josh <joli6799@uni.sydney.edu.au>
2024-04-30 15:30:16 +1000

    merge conflict

--- app/app/routes/server_routes.js
+++ app/app/routes/server_routes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
+const verifyToken = require('../auth.js');
 const user_controller = require("../controllers/userController");
 const char_controller = require('../controllers/charController');
 router.post(
     "/signup",
     user_controller.create_user
 );
+router.post(
+    "/login",
+    user_controller.login_user
+);
+
+router.get('/test', verifyToken, (req, res) => {
+    res.status(200).json({ message: 'route accessed' });
+});
 
 router.get('/', char_controller.getIndex);
 router.get('/allchar', char_controller.getAllChar);
