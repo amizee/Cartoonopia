b6478fca9907edfbc7cb52b08fbc07e961866df5
James <jqia4451@uni.sydney.edu.au>
2024-05-08 03:31:48 +1000

    [dev] basic nav bar, character contributed by bugfix

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import api from './api.js';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, useNavigate } from 'react-router-dom';
 
 import './static/css/App.css';
 
 
 function Home() {
+  const navigate = useNavigate();
 
 	return (
       <div className="App">
         <header className="App-header">
+        <div className="top-bar">
+          <h1 className="logo-title">Cartoonopia!</h1>
+          <button className="nav-button" onClick={() => navigate('/home')}>Go Home</button>
+          <button className="nav-button" onClick={() => navigate('/home')}>User Profile</button>
+          <button className="nav-button" onClick={() => navigate('/home')}>All Users</button>
+          <button className="nav-button" onClick={() => navigate('/allchar')}>All Characters</button>
+        </div>
           <h1>Welcome to My App</h1>
           <nav>
-            <Link to={`/allchar`}>All Characters</Link> <br />
             <Link to={`/contributions`}>Contributions</Link>
           </nav>
         </header>
